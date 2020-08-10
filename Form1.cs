b51d778b5f2b482715f3baa5c5467bb4c228d51c
John Uthus <john.a.uthus.jr1@gmail.com>
2020-08-09 17:35:51 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkCRUDApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Product p = new Product()
+            //{
+            //    Title = "Gadget",
+            //    Price = 9.99
+            //};
+
+            //ProductContext dbContext = new ProductContext();
+            //dbContext.Products.Add(p);
+            //dbContext.SaveChanges();
+            //MessageBox.Show("Added");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var p1 = new Product()
+            {
+                Title = "Widget",
+                Price = 5.99
+            };
+
+            ProductDB.Add(p1);
+
+            var p2 = new Product()
+            {
+                Title = "Gaming chair",
+                Price = 299
+            };
+
+            ProductDB.Add(p2);
+
+            p1.Price = 15.50;
+
+            ProductDB.Update(p1);
+
+            List<Product> prods = ProductDB.GetAllProducts();
+
+            ProductDB.Delete(p1);
+            ProductDB.Delete(p2);
+
+            prods = ProductDB.GetAllProducts();
+        }
+    }
+}
---
+++ ProductDB.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCRUDApp
+{
+    static class ProductDB
+    {
+        public static List<Product> GetAllProducts()
+        {
+            using (ProductContext context = new ProductContext())
+            {
+                // Query syntax
+                List<Product> allPods =
+                    (from prod in context.Products
+                     select prod).ToList();
+
+                // Method syntax
+                List<Product> allProds2 =
+                    context.Products.ToList();
+
+                return allPods;
+            }
+        }
+
+        /// <summary>
+        /// Adds a product to the database. Returns the product
+        /// with the <see cref="Product.ProductId" /> (Identity column) populated
+        /// </summary>
+        /// <param name="p">The product to be added</param>
+        /// <returns></returns>
+        public static Product Add(Product p)
+        {
+            using (ProductContext context = new ProductContext())
+            {
+                context.Products.Add(p); // Prepares Insert Query
+                context.SaveChanges(); // Executes all pending insert/update/delete queries
+
+                return p;
+            }
+        }
+
+        public static Product Update(Product p)
+        {
+            using (var context = new ProductContext())
+            {
+                context.Database.Log = Console.WriteLine;
+
+                // prepare update query for product
+                context.Entry(p).State = EntityState.Modified;
+
+                context.SaveChanges();
+
+                return p;
+            }
+        }
+
+        public static void Delete(Product p)
+        {
+
+            // try then finnaly except its using instead
+            // also calls dispose at the end
+            using (ProductContext context = new ProductContext())
+            {
+                // Write EF output to the console
+                context.Database.Log = Console.WriteLine;
+
+                // Telling EF that the product is already in the DB
+                // and it should be deleted
+                context.Entry(p).State = EntityState.Deleted;
+
+                // Executes Delete query
+                context.SaveChanges();
+            }
+        }
+
+        public static void Delete(int Id)
+        {
+            using (var context = new ProductContext())
+            {
+                // Get product to delete from database
+
+                // Query syntax
+                Product prodToDelete =
+                    (from p in context.Products
+                     where p.ProductId == Id
+                     select p).Single();
+
+                // Method syntax
+                Product prodToDelete2 =
+                    context.Products
+                           .Where(p => p.ProductId == Id)
+                           .Single();
+
+                //private bool Filter(Product p, int id)
+                //{
+                //    return p.ProductId == Id
+                //}
+
+
+                context.Entry(prodToDelete).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
